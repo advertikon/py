9a3e2f58d489280814c705fa41c17e7cd40c38a6
Maxim Bogochunas <bogochunas@gmail.com>
2019-04-10 16:12:55 +0300

    selenium

--- localogic.js
+++ localogic.js
@@ -1,16 +1,31 @@
 var util = require( "util" )
+// require('request')({
+//   method: 'GET',
+//   url: 'https://api.locallogic.co/v1/profiles/g30_c2uggt9f',
+//   qs: {
+//     lat: 49.910257,
+//     lng: -116.905032,
+//   },
+//   headers: {
+//     'X-API-KEY': 'EF0RCsVvLS5isoKB6UNph5U2IavwSeEF2fHOTvEp'
+//   }
+// }, function (err, response, body) {
+//    var data = JSON.parse( body );
+
+//    console.log( util.inspect( data, false, null, false ) );
+// });
+
 require('request')({
   method: 'GET',
-  url: 'https://api.locallogic.co/v1/profiles/g30_c2uggt9f',
+  url: 'https://api.locallogic.co/v1/demographics',
   qs: {
-    lat: 49.910257,
-    lng: -116.905032,
+    lat: 45.5017,
+    lng: -73.5656,
   },
   headers: {
     'X-API-KEY': 'EF0RCsVvLS5isoKB6UNph5U2IavwSeEF2fHOTvEp'
   }
 }, function (err, response, body) {
-   var data = JSON.parse( body );
-
-   console.log( util.inspect( data, false, null, false ) );
+  var data = JSON.parse( body );
+  console.log( util.inspect( data, false, null, true ) );
 })
--- sel.py
+++ sel.py
@@ -2,14 +2,66 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import re
+import sys
+
+import http.cookiejar
+import urllib.request
+import time
 
 driver = webdriver.Chrome()
 driver.get( "https://www.copart.com" )
 
-try:
-	element = WebDriverWait( driver, 5 ).until( EC.title_contains( "Copart" ) )
+sessionCookie = None
 
+try:
 	for c in driver.get_cookies():
-		print( c.get( "name" ), c.get( "value" ) )
+		if re.match( r'^incap_ses_', c.get( 'name' ) ):
+			sessionCookie = c
 finally:
 	driver.quit()
+
+if None == sessionCookie:
+	print( "Cookie not detected" )
+	sys.exit( 1 )
+
+print( sessionCookie )
+
+cookie = http.cookiejar.Cookie(
+	version = 0,
+	name = sessionCookie.get( 'name' ),
+	value = sessionCookie.get( 'value' ),
+	port = None,
+	port_specified = False,
+	domain = sessionCookie.get( 'domain' ),
+	domain_specified = True,
+	domain_initial_dot = False,
+	path = sessionCookie.get( 'path' ),
+	path_specified = True,
+	secure = sessionCookie.get( 'secure' ),
+	expires = None,
+	discard = None,
+	comment = None,
+	comment_url = None,
+	rest = True
+)
+
+cookieJar = http.cookiejar.CookieJar()
+cookieJar.set_cookie( cookie )
+
+headers = {
+	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+	"Accept-Encoding": "none",
+	"Accept-Language": "en",
+	"Connection": "keep-alive",
+	"DNT": "1",
+	"Upgrade-Insecure-Requests": "1",
+	"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"
+	# "Cookie": "incap_ses_729_242093=/j+dOgUFlA/+VmRrh+4dCrwanlwAAAAAEXf46pByn8cBLwU6WBJ+mQ=="
+}
+
+request  = urllib.request.Request( "https://www.copart.com", headers = headers )
+opener   = urllib.request.build_opener( urllib.request.HTTPCookieProcessor( cookieJar ) )
+response = opener.open( request )
+
+print( response.read().decode( "utf-8" ) )
